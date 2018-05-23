554382f4144389d22fd5ae1f96ba4e053c355fa7
copycat1024 <tranminhhien102408@gmail.com>
2018-05-23 05:08:58 +0300

    Add files to cbp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <fstream>
 #include "battle.h"
+#include "client.h"
 
 using std::ofstream;
 
 int main(void){
 	Battle b;
+	Client c("battle_log.lua");
 	b.run();
 	ofstream file;
 
@@ -12,5 +14,6 @@ int main(void){
 	for (auto t : b.get_log()){
 		file << t << '\n';
 	}
+	c.show();
 	return 0;
 }
